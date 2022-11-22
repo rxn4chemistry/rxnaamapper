1f00e9cf545841f5938498078418e4e5b5c267b6
Yves Gaetan Nana Teukam <nanayves@cccxl012.pok.ibm.com>
2022-11-22 08:41:22 -0500

    fix: fixed evaluation.
    Signed-off-by: Yves Gaetan Nana Teukam <nanayves@cccxl012.pok.ibm.com>

--- src/rxn_aa_mapper/training.py
+++ src/rxn_aa_mapper/training.py
@@ -1,6 +1,5 @@
 """Training utilities."""
 import os
-import subprocess
 from typing import Any, Dict, Union
 
 import pytorch_lightning as pl
@@ -17,68 +16,6 @@ from .dataset import (
     EnzymaticReactionLightningDataModule,
 )
 from .model import EnzymaticReactionLightningModule
-
-# CCC Specific Code --------------------------------------------------------------------------------------------------------------------------
-def fix_infiniband():
-    ibv = subprocess.run("ibv_devinfo", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    lines = ibv.stdout.decode("utf-8").split("\n")
-    exclude = ""
-    for line in lines:
-        if "hca_id:" in line:
-            name = line.split(":")[1].strip()
-        if "\tport:" in line:
-            port = line.split(":")[1].strip()
-        if "link_layer:" in line and "Ethernet" in line:
-            exclude = exclude + f"{name}:{port},"
-
-    if exclude:
-        exclude = "^" + exclude[:-1]
-        os.environ["NCCL_IB_HCA"] = exclude
-
-
-def set_env():
-    # print("Using " + str(torch.cuda.device_count()) + " GPUs---------------------------------------------------------------------")
-
-    LSB_MCPU_HOSTS = os.environ["LSB_MCPU_HOSTS"].split(
-        " "
-    )  # Parses Node list set by LSF, in format hostname proceeded by number of cores requested
-    HOST_LIST = LSB_MCPU_HOSTS[::2]  # Strips the cores per node items in the list
-    LSB_JOBID = os.environ[
-        "LSB_JOBID"
-    ]  # Parses Node list set by LSF, in format hostname proceeded by number of cores requested
-    os.environ["MASTER_ADDR"] = HOST_LIST[
-        0
-    ]  # Sets the MasterNode to thefirst node on the list of hosts
-    os.environ["MASTER_PORT"] = "5" + LSB_JOBID[-5:-1]
-    os.environ["NODE_RANK"] = str(
-        HOST_LIST.index(os.environ["HOSTNAME"])
-    )  # Uses the list index for node rank, master node rank must be 0
-    os.environ[
-        "NCCL_SOCKET_IFNAME"
-    ] = "ib,bond"  # avoids using docker of loopback interface
-    os.environ[
-        "NCCL_DEBUG"
-    ] = "INFO"  # sets NCCL debug to info, during distributed training, bugs in code show up as nccl errors
-    os.environ["NCCL_IB_CUDA_SUPPORT"] = "1"  # Force use of infiniband
-    os.environ["NCCL_TOPO_DUMP_FILE"] = "NCCL_TOP.%h.xml"
-    os.environ["NCCL_DEBUG_FILE"] = "NCCL_DEBUG.%h.%p.txt"
-    # print(os.environ["HOSTNAME"] + " MASTER_ADDR: " + os.environ["MASTER_ADDR"])
-    # print(os.environ["HOSTNAME"] + " MASTER_PORT: " + os.environ["MASTER_PORT"])
-    # print(os.environ["HOSTNAME"] + " NODE_RANK " + os.environ["NODE_RANK"])
-    # print(os.environ["HOSTNAME"] + " NCCL_SOCKET_IFNAME: " + os.environ["NCCL_SOCKET_IFNAME"])
-    # print(os.environ["HOSTNAME"] + " NCCL_DEBUG: " + os.environ["NCCL_DEBUG"])
-    # print(os.environ["HOSTNAME"] + " NCCL_IB_CUDA_SUPPORT: " + os.environ["NCCL_IB_CUDA_SUPPORT"])
-    # print("Using " + str(torch.cuda.device_count()) + " GPUs---------------------------------------------------------------------")
-    # print(os.environ["HOSTNAME"] + " LSB_MCPU_HOSTS: " + os.environ["LSB_MCPU_HOSTS"])
-    # print("Using " + str(torch.cuda.device_count()) + " GPUs---------------------------------------------------------------------")
-    # print(os.environ["HOSTNAME"] + " HOST_LIST: ")
-    # print(HOST_LIST)
-    # print("Using " + str(torch.cuda.device_count()) + " GPUs---------------------------------------------------------------------")
-    # print(os.environ["HOSTNAME"] + " HOSTNAME: " + os.environ["HOSTNAME"])
-    # print("Using " + str(torch.cuda.device_count()) + " GPUs---------------------------------------------------------------------")
-
-
-# --------------------------------------------------------------------------------------------------------------------------------------------
 
 
 def get_data_module(
@@ -116,11 +53,6 @@ def train(
         dataset_args: dictionary containing all the necessary parameters for the dataset creation.
         training_args: dictionary containing all the necessary parameters for the training routine.
     """
-
-    # CCC Specific Code --------------------------------------------------------------------------------------------------------------------------
-    fix_infiniband()
-    set_env()
-    # --------------------------------------------------------------------------------------------------------------------------------------------
 
     data_module = get_data_module(dataset_args)
     model_architecture["vocab_size"] = data_module.train_dataset.tokenizer.vocab_size
